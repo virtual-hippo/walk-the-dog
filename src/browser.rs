b590df45135dc9434eb079ced23e1034bc8aeb5c
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2024-10-19 19:18:15 +0900

    [chapter_3.2.4] Added the engine module.

--- src/browser.rs
+++ src/browser.rs
@@ -1,10 +1,13 @@
 use std::future::Future;
 
 use anyhow::{anyhow, Result};
-use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
+use wasm_bindgen::{
+    closure::{Closure, WasmClosureFnOnce},
+    prelude::*,
+    JsCast,
+};
 use wasm_bindgen_futures::JsFuture;
-use web_sys::{CanvasRenderingContext2d, Document, HtmlCanvasElement, Window};
+use web_sys::{CanvasRenderingContext2d, Document, HtmlCanvasElement, HtmlImageElement, Window};
 
 macro_rules! log {
     ($($t:tt)*) => {
@@ -68,4 +71,15 @@ pub async fn fetch_json(json_path: &str) -> Result<JsValue> {
     )
     .await
     .map_err(|err| anyhow!("error fetchng JSON {:#?}", err))
+}
+
+pub fn new_image() -> Result<HtmlImageElement> {
+    HtmlImageElement::new().map_err(|err| anyhow!("Could not create HtmlImageElement: {:#?}", err))
+}
+
+pub fn closure_once<F, A, R>(fn_once: F) -> Closure<F::FnMut>
+where
+    F: 'static + WasmClosureFnOnce<A, R>,
+{
+    Closure::once(fn_once)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 mod browser;
+mod engine;
 
 use std::{collections::HashMap, rc::Rc, sync::Mutex};
 
@@ -47,30 +48,9 @@ pub fn main_js() -> Result<(), JsValue> {
             .into_serde()
             .expect("Could not convert rhb.json into a Sheet structure");
 
-        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
-        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
-        let error_tx = Rc::clone(&success_tx);
-
-        let image = web_sys::HtmlImageElement::new().unwrap();
-
-        let callback = Closure::once(move || {
-            if let Some(sender) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                let _ = sender.send(Ok(()));
-            }
-        });
-
-        let error_callback = Closure::once(move |err| {
-            if let Some(sender) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                let _ = sender.send(Err(err));
-            }
-        });
-
-        image.set_onload(Some(callback.as_ref().unchecked_ref()));
-        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
-
-        image.set_src("rhb.png");
-
-        let _ = success_rx.await;
+        let image = engine::load_image("rhb.png")
+            .await
+            .expect("Could not load rhb.png");
 
         let mut frame: u8 = 7;
         let interval_callback = Closure::wrap(Box::new(move || {
